1a34a9d40106d8557e566348fa8d444105f57317
BastianFig <bastian.figueroa1@mail.udp.cl>
2024-11-27 19:27:55 -0300

    solo agrego ip

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,8 @@ const allowedOrigins = [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:3001',
-    'http://127.0.0.1:3001'
+    'http://127.0.0.1:3001',
+    'http://10.0.0.104:3000',
 ];
 
 app.use(
